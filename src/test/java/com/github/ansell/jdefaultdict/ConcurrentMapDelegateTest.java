7007194b26c7ff79ef1849d6872d36fb0d32fd06
Peter Ansell <p_ansell@yahoo.com>
2015-03-01 20:07:26 +1100

    Add stub ConcurrentMapDelegateTest

---
+++ src/test/java/com/github/ansell/jdefaultdict/ConcurrentMapDelegateTest.java
@@ -0,0 +1,341 @@
+/**
+ * 
+ */
+package com.github.ansell.jdefaultdict;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Tests for {@link ConcurrentMapDelegate}.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public class ConcurrentMapDelegateTest {
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#hashCode()}.
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testHashCode() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#ConcurrentMapDelegate(java.util.concurrent.ConcurrentMap)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testConcurrentMapDelegateConcurrentMapOfKV()
+			throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#ConcurrentMapDelegate(java.util.function.Supplier)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testConcurrentMapDelegateSupplierOfConcurrentMapOfKV()
+			throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#checkDelegate()}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testCheckDelegate() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#clear()}.
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testClear() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#clone()}.
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testClone() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#compute(java.lang.Object, java.util.function.BiFunction)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testCompute() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#computeIfAbsent(java.lang.Object, java.util.function.Function)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testComputeIfAbsent() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#computeIfPresent(java.lang.Object, java.util.function.BiFunction)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testComputeIfPresent() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#containsKey(java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testContainsKey() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#containsValue(java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testContainsValue() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#entrySet()}.
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testEntrySet() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#equals(java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testEqualsObject() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#forEach(java.util.function.BiConsumer)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testForEach() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#get(java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testGet() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#getOrDefault(java.lang.Object, java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testGetOrDefault() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#isEmpty()}.
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testIsEmpty() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#keySet()}.
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testKeySet() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#merge(java.lang.Object, java.lang.Object, java.util.function.BiFunction)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testMerge() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#put(java.lang.Object, java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testPut() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#putAll(java.util.Map)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testPutAll() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#putIfAbsent(java.lang.Object, java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testPutIfAbsent() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#remove(java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testRemoveObject() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#remove(java.lang.Object, java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testRemoveObjectObject() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#replace(java.lang.Object, java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testReplaceKV() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#replace(java.lang.Object, java.lang.Object, java.lang.Object)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testReplaceKVV() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#replaceAll(java.util.function.BiFunction)}
+	 * .
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testReplaceAll() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#size()}.
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testSize() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#toString()}.
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testToString() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.ansell.jdefaultdict.ConcurrentMapDelegate#values()}.
+	 */
+	@Ignore("TODO: Implement me")
+	@Test
+	public final void testValues() throws Exception {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
